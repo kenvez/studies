a49919b02d643b7149fa511ccc9b8422e7fc1ad8
kenvez <kenvezdev@proton.me>
2024-11-02 16:00:43 +0100

    ex. 03 operators, variables, and numeral systems

---
+++ 1-semester/ppj/ex. 03 operators, variables, and numeral systems/Exercise2.java
@@ -0,0 +1,35 @@
+public class Exercise2 {
+	public static void main(String[] args) {
+		double var1 = .2;
+    double var2 = 16.3;
+    int var3 = 178;
+    long var4 = 4L;
+    double var5 = 567d;
+    float var6 = 15.87F;
+    boolean var7 = false;
+    String var8 = "PPJ";
+    char var9 = 'A';
+    int var10 = 0b1_0101;
+    int var11 = 0b10101;
+    int var12 = 0120;
+    int var13 = 0x2F;
+    char var14 = '\u0041';
+    double var15 = 0.031415e2; 
+
+    System.out.println(var1);
+    System.out.println(var2);
+    System.out.println(var3);
+    System.out.println(var4);
+    System.out.println(var5);
+    System.out.println(var6);
+    System.out.println(var7);
+    System.out.println(var8);
+    System.out.println(var9);
+    System.out.println(var10);
+    System.out.println(var11);
+    System.out.println(var12);
+    System.out.println(var13);
+    System.out.println(var14);
+    System.out.println(var15);
+	}
+}
