4f9a692f88c3eab04ed96c7e32c9d7612c964470
kenvez <kenvezdev@proton.me>
2024-11-04 21:01:06 +0100

    ex-03-operators-variables-and-numeral-systems

--- 1-semester/ppj/ex-03-operators-variables-and-numeral-systems/Exercise1.java
+++ 1-semester/ppj/ex-03-operators-variables-and-numeral-systems/Exercise1.java
@@ -1,7 +1,7 @@
 public class Exercise1 {
 	public static void main(String[] args) {
-        double x = (1 + 2 * 3 + 4) / (5.0 - 6 + 9) + (78.0 / 10 - 10.0 / 11);
+        	double x = (1 + 2 * 3 + 4) / (5.0 - 6 + 9) + (78.0 / 10 - 10.0 / 11);
 
-        System.out.println(x);
+        	System.out.println(x);
 	}
 }
