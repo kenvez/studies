4e068ea5e13b67ce8ffeb905539080ac0adc2688
kenvez <kenvezdev@proton.me>
2024-11-04 20:55:26 +0100

    ex-03-operators-variables-and-numeral-systems

---
+++ 1-semester/ppj/ex-03-operators-variables-and-numeral-systems/Exercise3.java
@@ -0,0 +1,19 @@
+public class Exercise3 {
+	public static void main(String[] args) {
+		byte byteVar = 10;
+        char charVar = 'A';
+        int intVar = 100;
+        float floatVar = 5.5f;
+        double doubleVar = 3.14;
+
+        int charResult = charVar + intVar;
+        int intResult = intVar + charVar;
+        double floatResult = floatVar + doubleVar;
+        int byteResult = byteVar + intVar;
+
+        System.out.println("charResult (char + int) = " + charResult);
+        System.out.println("intResult (int + char) = " + intResult);
+        System.out.println("floatResult (float + double) = " + floatResult);
+        System.out.println("byteResult (byte + int) = " + byteResult);
+	}
+}
--- 1-semester/ppj/ex. 03 operators, variables, and numeral systems/Exercise1.java
+++
@@ -1,7 +0,0 @@
-public class Exercise1 {
-	public static void main(String[] args) {
-    double x = (1 + 2 * 3 + 4) / (5.0 - 6 + 9) + (78.0 / 10 - 10.0 / 11);		
-		
-    System.out.println(x);
-	}
-}
